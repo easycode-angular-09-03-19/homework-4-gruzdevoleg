1fc85f8ffb0957b4ad1fe6ec80943d52a9f27f3c
Oleg Gruzdev <oleg.gruzdev.1982@gmail.com>
2019-03-26 22:30:39 +0200

    homework_4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+//import { TaskFormComponent } from './components/task-form/task-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    //TaskFormComponent
   ],
   imports: [
     BrowserModule
